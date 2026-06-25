edafc70724cd9fde5b800838b4779df90756fea9
Brent <43089001+BrentDaMage@users.noreply.github.com>
2026-06-25 04:20:09 -0500

    Fixed VS platform toolset version auto-selection * Disabled classic crafting by default on Console

--- source/client/options/Options.cpp
+++ source/client/options/Options.cpp
@@ -96,7 +96,7 @@ Options::Options(Minecraft* mc, const std::string& folderPath) :
 	, m_uiTheme("gfx_uitheme", "options.uiTheme", GetDefaultUiTheme(m_pMinecraft), ValuesBuilder().add("options.uiTheme.pocket").add("options.uiTheme.java").add("options.uiTheme.console"))
 	, m_logoType("gfx_logotype", "options.logoType", LOGO_AUTO, ValuesBuilder().add("options.logoType.auto").add("options.logoType.pocket").add("options.logoType.java").add("options.logoType.console").add("options.logoType.xbox360").add("options.logoType.logo3d"))
 	, m_hudSize("gfx_hudsize", "options.hudSize", HUD_SIZE_2)
-	, m_classicCrafting("gfx_classiccrafting", "options.classicCrafting", true)
+	, m_classicCrafting("gfx_classiccrafting", "options.classicCrafting", false)
 	, m_animatedCharacter("gfx_animatedcharacter", "options.animatedCharacter", true)
 	//, m_limitFramerate("gfx_fpslimit", "options.framerateLimit", 0, ValuesBuilder().add(performance.max").add("performance.balanced").add("performance.powersaver"))
 	//, m_bMipmaps("gfx_mipmaps", "options.mipmaps")
