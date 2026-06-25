507fffd232e25c4bbf999e92c2f3a58ab691bf7b
Wilyicaro <81383021+Wilyicaro@users.noreply.github.com>
2026-06-25 20:23:25 -0300

    CompassTexture and ClockTexture (#637)

---
+++ source/client/renderer/ClockTexture.cpp
@@ -0,0 +1,92 @@
+#include "DynamicTexture.hpp"
+#include "world/item/Item.hpp"
+#include "client/app/Minecraft.hpp"
+
+ClockTexture::ClockTexture(Minecraft* minecraft) : DynamicTexture(Item::clock->m_icon), m_pMinecraft(minecraft), m_rot(0), m_rota(0)
+{
+    m_dialData = minecraft->m_pTextures->getTextureData("misc/dial.png", true)->getData();
+    m_textureId = 1;
+
+    TextureData* texture = minecraft->m_pTextures->getTextureData(C_ITEMS_NAME, true);
+
+    for (int i = 0; i < 256; i++)
+    {
+        int x = i % 16;
+        int y = i / 16;
+
+        int texX = (m_textureIndex % 16) * 16 + x;
+        int texY = (m_textureIndex / 16) * 16 + y;
+
+        m_data[i] = texture->getData()[texY * texture->m_imageData.m_width + texX];
+    }
+}
+
+void ClockTexture::tick()
+{
+    float rott = 0.0f;
+    if (m_pMinecraft->m_pLevel && m_pMinecraft->m_pLocalPlayer)
+    {
+        float time = m_pMinecraft->m_pLevel->getTimeOfDay(1.0f);
+        rott = (float)(-time * M_PI * 2.0f);
+        if (m_pMinecraft->m_pLevel->m_pDimension->m_bFoggy)
+            rott = Mth::random() * M_PI * 2.0f;
+    }
+
+    float rotd;
+    for (rotd = rott - m_rot; rotd < -M_PI; rotd += 2 * M_PI) {
+    }
+
+    while (rotd >= M_PI)
+        rotd -= 2 * M_PI;
+
+    if (rotd < -1.0f)
+        rotd = -1.0f;
+
+    if (rotd > 1.0f)
+        rotd = 1.0f;
+
+    m_rota += rotd * 0.1f;
+    m_rota *= 0.8;
+    m_rot += m_rota;
+    float sin = Mth::sin(m_rot);
+    float cos = Mth::cos(m_rot);
+
+    for (int i = 0; i < 256; ++i)
+    {
+        int a = m_data[i] >> 24 & 255;
+        int r = m_data[i] >> 16 & 255;
+        int g = m_data[i] >> 8 & 255;
+        int b = m_data[i] >> 0 & 255;
+        //@NOTE Temporary fix
+        std::swap(r, b);
+        if (r == b && g == 0 && b > 0)
+        {
+            float xo = -((float)(i % 16) / 15.0f - 0.5f);
+            float yo = (float)(i / 16) / 15.0f - 0.5f;
+            int br = r;
+            int x = (int)((xo * cos + yo * sin + 0.5f) * 16.0f);
+            int y = (int)((yo * cos - xo * sin + 0.5f) * 16.0f);
+            int j = (x & 15) + (y & 15) * 16;
+            a = m_dialData[j] >> 24 & 255;
+            r = (m_dialData[j] >> 16 & 255) * r / 255;
+            g = (m_dialData[j] >> 8 & 255) * br / 255;
+            b = (m_dialData[j] >> 0 & 255) * br / 255;
+            std::swap(r, b);
+        }
+
+        if (m_bAnaglyph3d)
+        {
+            int rr = (r * 30 + g * 59 + b * 11) / 100;
+            int gg = (r * 30 + g * 70) / 100;
+            int bb = (r * 30 + b * 70) / 100;
+            r = rr;
+            g = gg;
+            b = bb;
+        }
+
+        m_pixels[i * 4 + 0] = r;
+        m_pixels[i * 4 + 1] = g;
+        m_pixels[i * 4 + 2] = b;
+        m_pixels[i * 4 + 3] = a;
+    }
+}
---
+++ source/client/renderer/PortalTexture.cpp
@@ -0,0 +1,82 @@
+#include "DynamicTexture.hpp"
+//#include "world/tile/PortalTile.hpp"
+
+PortalTexture::PortalTexture() : DynamicTexture(/*Tile::portal->m_TextureFrame*/+0), m_time(0)
+{
+    Random random(100);
+
+    for (int time = 0; time < 32; ++time)
+    {
+        for (int x = 0; x < 16; ++x)
+        {
+            for (int y = 0; y < 16; ++y)
+            {
+                float pow = 0.0f;
+
+                for (int i = 0; i < 2; ++i)
+                {
+                    float xo = (float)(i * 8);
+                    float yo = (float)(i * 8);
+                    float xd = ((float)x - xo) / 16.0f * 2.0f;
+                    float yd = ((float)y - yo) / 16.0f * 2.0f;
+                    if (xd < -1.0f)
+                        xd += 2.0f;
+
+                    if (xd >= 1.0f)
+                        xd -= 2.0f;
+
+                    if (yd < -1.0f)
+                        yd += 2.0f;
+
+                    if (yd >= 1.0f)
+                        yd -= 2.0f;
+
+                    float dd = xd * xd + yd * yd;
+                    float pp = Mth::atan2((float)yd, (float)xd) + ((float)time / 32.0f * M_PI * 2.0f - dd * 10.0f + (float)(i * 2)) * (float)(i * 2 - 1);
+                    pp = (Mth::sin(pp) + 1.0f) / 2.0f;
+                    pp /= dd + 1.0f;
+                    pow += pp * 0.5f;
+                }
+
+                pow += random.nextFloat() * 0.1f;
+                int r = (int)(pow * pow * 200.0f + 55.0f);
+                int g = (int)(pow * pow * pow * pow * 255.0f);
+                int b = (int)(pow * 100.0f + 155.0f);
+                int a = (int)(pow * 100.0f + 155.0f);
+                int i = y * 16 + x;
+                m_frames[time][i * 4 + 0] = r;
+                m_frames[time][i * 4 + 1] = g;
+                m_frames[time][i * 4 + 2] = b;
+                m_frames[time][i * 4 + 3] = a;
+            }
+        }
+    }
+}
+
+void PortalTexture::tick()
+{
+    ++m_time;
+    uint8_t* source = m_frames[m_time & 31];
+
+    for (int i = 0; i < 256; ++i)
+    {
+        int r = source[i * 4 + 0] & 255;
+        int g = source[i * 4 + 1] & 255;
+        int b = source[i * 4 + 2] & 255;
+        int a = source[i * 4 + 3] & 255;
+        if (m_bAnaglyph3d) 
+        {
+            int rr = (r * 30 + g * 59 + b * 11) / 100;
+            int gg = (r * 30 + g * 70) / 100;
+            int bb = (r * 30 + b * 70) / 100;
+            r = rr;
+            g = gg;
+            b = bb;
+        }
+
+        m_pixels[i * 4 + 0] = r;
+        m_pixels[i * 4 + 1] = g;
+        m_pixels[i * 4 + 2] = b;
+        m_pixels[i * 4 + 3] = a;
+    }
+}
