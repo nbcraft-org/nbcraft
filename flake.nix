c7afa93c0d3c0e4337e9158c20bd1563ae76b64b
MolassesLover <mail@molasses.love>
2026-06-28 15:41:35 +0200

    Fixed some oversights, and tidied things up

--- flake.nix
+++ flake.nix
@@ -1,23 +1,32 @@
 {
-  description =
-    "An unofficial, community run, and open-source re-implementation of Minecraft: Pocket Edition (v0.1.3). Unaffiliated with Mojang.";
+  description = "The ultimate cross-platform mining experience.";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    in {
-      packages = forAllSystems (system:
+    in
+    {
+      packages = forAllSystems (
+        system:
         let
           pkgs = (import nixpkgs { inherit system; });
-          nbcraft = pkgs.callPackage misc/nix/package.nix { };
-        in {
+          nbcraft = pkgs.callPackage platforms/nix/package.nix { };
+        in
+        {
           inherit nbcraft;
           default = nbcraft;
           devShell = pkgs.mkShell { inputsFrom = [ nbcraft ]; };
-        });
+        }
+      );
     };
 }
