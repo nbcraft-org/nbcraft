0c95403d297fadbf629dcb15139f009ff19cfbbc
Shredder <fileshreddervm@gmail.com>
2026-06-30 03:32:16 +0500

    Added Level Seed reading crap

--- source/world/level/storage/LevelStorageSource.hpp
+++ source/world/level/storage/LevelStorageSource.hpp
@@ -16,6 +16,7 @@ struct LevelSummary
 	std::string m_levelName;
 	int m_lastPlayed;
 	int m_sizeOnDisk;
+	int m_levelSeed;
 	GameType m_gameType;
     unsigned int m_storageVersion;
 
@@ -23,18 +24,20 @@ struct LevelSummary
 	{
 		m_lastPlayed = 0;
 		m_sizeOnDisk = 0;
+		m_levelSeed = 0;
 		m_gameType = GAME_TYPE_CREATIVE;
         m_storageVersion = LEVEL_STORAGE_VERSION_DEFAULT;
 	}
 
-	LevelSummary(const std::string& fileName, const std::string& levelName, int lastPlayed, int sizeOnDisk, GameType gameType = GAME_TYPE_CREATIVE, unsigned int storageVersion = LEVEL_STORAGE_VERSION_DEFAULT)
+	LevelSummary(const std::string& fileName, const std::string& levelName, int lastPlayed, int sizeOnDisk, int levelSeed, GameType gameType = GAME_TYPE_CREATIVE, unsigned int storageVersion = LEVEL_STORAGE_VERSION_DEFAULT)
 	{
 		m_fileName = fileName;
 		m_levelName = levelName;
 		m_lastPlayed = lastPlayed;
 		m_sizeOnDisk = sizeOnDisk;
+		m_levelSeed = levelSeed;
 		m_gameType = gameType;
-        m_storageVersion = storageVersion;
+		m_storageVersion = storageVersion;
 	}
 
 	bool operator<(const LevelSummary& b) const
