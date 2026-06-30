bbc4ecf08e0bc3341ae36ed4850950a6c8864290
Matt <97983689+bluepilledgreat@users.noreply.github.com>
2026-06-30 01:42:45 +0100

    fix names

--- source/world/level/storage/DBStorage.cpp
+++ source/world/level/storage/DBStorage.cpp
@@ -1,19 +1,19 @@
 #include "world/level/storage/DBStorage.hpp"
 #include <leveldb/status.h>
 
-static LevelStorageState ConvertError(const leveldb::Status& status)
+static LevelStorage::Status ConvertError(const leveldb::Status& status)
 {
-	static const LevelStorageState levelStorageStatusMap[] = {
-		LEVELSTORAGESTATUS_OPEN,              // leveldb::Status::kOk
-		LEVELSTORAGESTATUS_NOT_FOUND,         // leveldb::Status::kNotFound
-		LEVELSTORAGESTATUS_CORRUPTED,         // leveldb::Status::kCorruption
-		LEVELSTORAGESTATUS_NOT_SUPPORTED,     // leveldb::Status::kNotSupported
-		LEVELSTORAGESTATUS_INVALID_ARGUMENTS, // leveldb::Status::kInvalidArgument
-		LEVELSTORAGESTATUS_IO_ERROR           // leveldb::Status::kIOError
+	static const LevelStorage::Status levelStorageStatusMap[] = {
+		LevelStorage::STATUS_OPEN,              // leveldb::Status::kOk
+		LevelStorage::STATUS_NOT_FOUND,         // leveldb::Status::kNotFound
+		LevelStorage::STATUS_CORRUPTED,         // leveldb::Status::kCorruption
+		LevelStorage::STATUS_NOT_SUPPORTED,     // leveldb::Status::kNotSupported
+		LevelStorage::STATUS_INVALID_ARGUMENTS, // leveldb::Status::kInvalidArgument
+		LevelStorage::STATUS_IO_ERROR           // leveldb::Status::kIOError
 	};
 
 	if (status.code() > leveldb::Status::kIOError)
-		return LEVELSTORAGESTATUS_UNKNOWN;
+		return LevelStorage::STATUS_UNKNOWN;
 
 	return levelStorageStatusMap[status.code()];
 }
