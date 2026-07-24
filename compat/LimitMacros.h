4b4e084f72e4363825e5768a367c6dddc6929fef
Brent <43089001+BrentDaMage@users.noreply.github.com>
2026-07-24 02:08:42 -0500

    Added redundancy for missing limit macros (#674)

--- compat/LimitMacros.h
+++ compat/LimitMacros.h
@@ -3,6 +3,10 @@
 #define __STDC_LIMIT_MACROS
 #include <stdint.h>
 
+#ifndef INT32_MAX
+#define INT32_MAX 2147483647
+#endif
+
 #ifndef UINT64_MAX
-#define UINT64_MAX 0xFFFFFFFFFFFFFFFFULL
+#define UINT64_MAX ((uint64_t) -1)
 #endif
