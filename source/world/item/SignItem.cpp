740a821a9e5499101f54e1a06777e5e0852c7932
Vimdo <minecraftjb22@gmail.com>
2026-08-03 18:25:02 -0400

    fix: remove if and else for sign facing

--- source/world/item/SignItem.cpp
+++ source/world/item/SignItem.cpp
@@ -22,10 +22,9 @@ bool SignItem::useOn(ItemStack* stack, Player* player, Level* level, const TileP
     if (!Tile::sign->mayPlace(level, tp))
         return false;
 
-    if (face == Facing::UP)
-        level->setTileAndData(tp, Tile::sign->m_ID, Mth::floor(((player->m_rot.yaw + 180.0f) * 16.0f / 360.0f) + 0.5) & 15);
-    else
-        level->setTileAndData(tp, Tile::wallSign->m_ID, face);
+    TileID tile = (face == Facing::UP) ? Tile::sign->m_ID : Tile::wallSign->m_ID;
+    TileData data = (face == Facing::UP) ? (TileData)(Mth::floor(((player->m_rot.yaw + 180.0f) * 16.0f / 360.0f) + 0.5) & 15) : (TileData)face;
+    level->setTileAndData(tp, tile, data);
 
     --stack->m_count;
     SignTileEntity* sign = dynamic_cast<SignTileEntity*>(level->getTileEntity(tp));
